406d97bc0fa78876053d14e67dbdb8c6156c1a35
Zitzabis <zitzabis.zitz@gmail.com>
2018-03-27 14:16:36 -0400

    Adds more CORS stuff.

--- matcher.go
+++ matcher.go
@@ -27,6 +27,12 @@ func main() {
 	// Allow all orgins
 	r.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
+		//c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
 		c.Next()
 	})
 
