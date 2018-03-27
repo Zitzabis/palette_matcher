d1deb79db446e4be2b2faa09be1458c954ba4390
Zitzabis <zitzabis.zitz@gmail.com>
2018-03-27 17:04:51 -0400

    Adds settings for OPTIONS because Chrome.

--- matcher.go
+++ matcher.go
@@ -28,12 +28,17 @@ func main() {
 	r.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		c.Writer.Header().Set("Access-Control-Expose-Headers", "Content-Length")
 		//c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 
-		c.Next()
+		if c.Request.Method == "OPTIONS" {
+             		fmt.Println("OPTIONS")
+             		c.AbortWithStatus(200)
+         	} else {
+             		c.Next()
+         	}
 	})
 
 	// Simple group: v1
