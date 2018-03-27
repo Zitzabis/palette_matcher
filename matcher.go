66682e0f12267e971768dd1d3a68825577223de4
Stephen F (Zitzabis) <zitzabis.zitz@gmail.com>
2018-03-27 02:06:20 -0400

    Checks head against palette. Also returns actual data now.

--- matcher.go
+++ matcher.go
@@ -34,10 +34,10 @@ func main() {
 				c.AbortWithError(400, err)
 				return
 			}
-			checkMatch(data)
+			match := checkMatch(data)
 			c.JSON(200, gin.H{
-				"isMatch":   true, // Was the image matched with only colors provided
-				"numColors": 5,    // How many colors exist (this can be used a verification)
+				"isMatch":   match, // Was the image matched with only colors provided
+				"numColors": 5,     // How many colors exist (this can be used a verification)
 			})
 		})
 
@@ -48,21 +48,68 @@ func main() {
 			})
 		})
 	}
+
 	r.Run(":3000") // listen and serve on 0.0.0.0:3000
 }
 
-func checkMatch(info *Data) {
+func checkMatch(info *Data) bool {
 	// You can register another format here
 	image.RegisterFormat("png", "png", png.Decode, png.DecodeConfig)
 
 	image := downloadData(info.URL)
+	dim := image.Bounds()
 
-	fmt.Println(colorAt(image, 0, 8))
-	fmt.Println(colorAt(image, 0, 9))
+	if !checkStandard(info, image) {
+		return false
+	}
+	if dim.Max.Y == 64 {
+		checkExtra()
+	}
+	return true
 }
 
 func countColors() {
 
+}
+
+func checkStandard(info *Data, img image.Image) bool {
+	// Head
+	for y := 0; y < 8; y++ {
+		for x := 8; x < 24; x++ {
+			if !contains(info, colorAt(img, x, y)) {
+				fmt.Println("Error at ", x, ", ", y, " - ", colorAt(img, x, y))
+				return false
+			}
+		}
+		for x := 40; x < 56; x++ {
+			if !contains(info, colorAt(img, x, y)) {
+				fmt.Println("Error at ", x, ", ", y, " - ", colorAt(img, x, y))
+				return false
+			}
+		}
+	}
+	for y := 8; y < 16; y++ {
+		for x := 0; x < 64; x++ {
+			if !contains(info, colorAt(img, x, y)) {
+				fmt.Println("Error at ", x, ", ", y, " - ", colorAt(img, x, y))
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func checkExtra() {
+
+}
+
+func contains(info *Data, str string) bool {
+	for _, code := range info.Hexs {
+		if code == str || str == "000" {
+			return true
+		}
+	}
+	return false
 }
 
 func downloadData(url string) image.Image {
@@ -103,7 +150,7 @@ func downloadData(url string) image.Image {
 }
 
 func colorAt(img image.Image, x int, y int) string {
-	rU, gU, bU, aU := img.At(y, x).RGBA()
+	rU, gU, bU, aU := img.At(x, y).RGBA()
 	r, g, b, _ := int(rU/257), int(gU/257), int(bU/257), int(aU/257)
 	hex := fmt.Sprintf("%x", r) + fmt.Sprintf("%x", g) + fmt.Sprintf("%x", b)
 	return s.ToUpper(hex)
